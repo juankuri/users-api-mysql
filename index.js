9fc2726824d47ab5da29c957dccb0f98e861de5e
juan kuri <juankuri@Kuri.KuriDomain>
2025-03-28 10:45:46 -0600

    dockerfile

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
