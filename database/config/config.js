22b83698553de17c73f8f173ecbe06e32141eada
kuri <juankupa11@gmail.com>
2025-05-01 22:49:59 -0600

    server y db config

--- database/config/config.js
+++ database/config/config.js
@@ -4,13 +4,31 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    },
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    },
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    },
   },
 };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const routes = require('../routes'); 
+const routes = require('../routes/userRoutes'); 
 
 const server = express();
 const PORT = process.env.PORT || 3300;
