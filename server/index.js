5f27ac509b85b43dcfc99dc31aee3e8a00da3f40
kuri <juankupa11@gmail.com>
2025-05-01 22:37:48 -0600

    server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-const cors = require('cors');
 const express = require('express');
-const routes = require('../routes/userRoutes');
+const cors = require('cors');
+const path = require('path');
+const routes = require('../routes'); 
 
 const server = express();
-server.use(cors());
-server.options('*', cors());
+const PORT = process.env.PORT || 3300;
+
 server.use(express.json());
-server.use('/api', routes);
+server.use(express.urlencoded({ extended: true }));
+
+server.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+server.use('/frontend', express.static(path.join(__dirname, '../frontend')));
+
+server.use('/api', routes); 
+
+server.use((err, req, res, next) => {
+  console.error('Error en el servidor:', err);
+  res.status(500).json({ error: 'Error interno del servidor' });
+});
 
 module.exports = server;
